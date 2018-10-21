53134e4fbf5f77895bcec1950c9467f2676dd787
CaramelYo_CGVSL_PC <alltime326@gmail.com>
2018-10-21 17:50:16 +0800

    first commit

---
+++ Typing_test/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Typing_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.restart_btn = new System.Windows.Forms.Button();
+            this.HP_label = new System.Windows.Forms.Label();
+            this.score_label = new System.Windows.Forms.Label();
+            this.name_label = new System.Windows.Forms.Label();
+            this.name_textbox = new System.Windows.Forms.TextBox();
+            this.name_btn = new System.Windows.Forms.Button();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.log_textbox = new System.Windows.Forms.TextBox();
+            this.deadline_textbox = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(29, 464);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 23);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // restart_btn
+            // 
+            this.restart_btn.Location = new System.Drawing.Point(151, 464);
+            this.restart_btn.Name = "restart_btn";
+            this.restart_btn.Size = new System.Drawing.Size(75, 23);
+            this.restart_btn.TabIndex = 1;
+            this.restart_btn.Text = "restart";
+            this.restart_btn.UseVisualStyleBackColor = true;
+            this.restart_btn.Click += new System.EventHandler(this.restart_btn_Click);
+            // 
+            // HP_label
+            // 
+            this.HP_label.AutoSize = true;
+            this.HP_label.Location = new System.Drawing.Point(255, 464);
+            this.HP_label.Name = "HP_label";
+            this.HP_label.Size = new System.Drawing.Size(22, 12);
+            this.HP_label.TabIndex = 2;
+            this.HP_label.Text = "HP:";
+            // 
+            // score_label
+            // 
+            this.score_label.AutoSize = true;
+            this.score_label.Location = new System.Drawing.Point(321, 464);
+            this.score_label.Name = "score_label";
+            this.score_label.Size = new System.Drawing.Size(32, 12);
+            this.score_label.TabIndex = 3;
+            this.score_label.Text = "score:";
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Font = new System.Drawing.Font("PMingLiU", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.name_label.Location = new System.Drawing.Point(595, 19);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(64, 19);
+            this.name_label.TabIndex = 4;
+            this.name_label.Text = "player: ";
+            // 
+            // name_textbox
+            // 
+            this.name_textbox.Location = new System.Drawing.Point(801, 21);
+            this.name_textbox.Name = "name_textbox";
+            this.name_textbox.Size = new System.Drawing.Size(100, 22);
+            this.name_textbox.TabIndex = 5;
+            // 
+            // name_btn
+            // 
+            this.name_btn.Location = new System.Drawing.Point(936, 19);
+            this.name_btn.Name = "name_btn";
+            this.name_btn.Size = new System.Drawing.Size(75, 23);
+            this.name_btn.TabIndex = 6;
+            this.name_btn.Text = "modify name";
+            this.name_btn.UseVisualStyleBackColor = true;
+            this.name_btn.Click += new System.EventHandler(this.name_btn_Click);
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Location = new System.Drawing.Point(936, 464);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(75, 23);
+            this.clear_btn.TabIndex = 7;
+            this.clear_btn.Text = "clear scores";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // log_textbox
+            // 
+            this.log_textbox.Location = new System.Drawing.Point(599, 70);
+            this.log_textbox.Multiline = true;
+            this.log_textbox.Name = "log_textbox";
+            this.log_textbox.ReadOnly = true;
+            this.log_textbox.Size = new System.Drawing.Size(412, 367);
+            this.log_textbox.TabIndex = 8;
+            // 
+            // deadline_textbox
+            // 
+            this.deadline_textbox.BackColor = System.Drawing.SystemColors.WindowText;
+            this.deadline_textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.deadline_textbox.Enabled = false;
+            this.deadline_textbox.Location = new System.Drawing.Point(4, 422);
+            this.deadline_textbox.Name = "deadline_textbox";
+            this.deadline_textbox.Size = new System.Drawing.Size(589, 15);
+            this.deadline_textbox.TabIndex = 9;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 521);
+            this.Controls.Add(this.deadline_textbox);
+            this.Controls.Add(this.log_textbox);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.name_btn);
+            this.Controls.Add(this.name_textbox);
+            this.Controls.Add(this.name_label);
+            this.Controls.Add(this.score_label);
+            this.Controls.Add(this.HP_label);
+            this.Controls.Add(this.restart_btn);
+            this.Controls.Add(this.start_btn);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button restart_btn;
+        private System.Windows.Forms.Label HP_label;
+        private System.Windows.Forms.Label score_label;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.TextBox name_textbox;
+        private System.Windows.Forms.Button name_btn;
+        private System.Windows.Forms.Button clear_btn;
+        private System.Windows.Forms.TextBox log_textbox;
+        private System.Windows.Forms.TextBox deadline_textbox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Typing_test/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace Typing_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            current_cubes = new Dictionary<char, List<Label>>();
+
+            r = new Random();
+
+            deadline_textbox_cube_right = deadline_textbox.Right - cube_size.Width;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Player_name = name_textbox.Text;
+            HP = init_HP;
+            Score = 0;
+            // 0 => game is stopped 1 => game start
+            game_state = 0;
+            stored_time = 0;
+
+            foreach (KeyValuePair<char, List<Label>> pair in current_cubes)
+            {
+                for (int i = 0; i < pair.Value.Count; ++i)
+                    pair.Value[i].Dispose();
+
+                pair.Value.Clear();
+            }
+
+            current_cubes.Clear();
+
+            current_cubes = new Dictionary<char, List<Label>>();
+
+            start_btn.Enabled = true;
+
+            FileInfo finfo = new FileInfo(score_path);
+            StreamWriter sw = finfo.AppendText();
+            sw.Close();
+
+            log_textbox.Text = "";
+            StreamReader sr = new StreamReader(score_path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] line_split = line.Split(' ');
+                log_textbox.Text += score_log(
+                    line_split[0],
+                    int.Parse(line_split[1]),
+                    System.DateTime.ParseExact(line_split[2] + " " + line_split[3], "yyyy/M/d HH:mm:ss", CultureInfo.InvariantCulture)
+                );
+            }
+            sr.Close();
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            switch (game_state)
+            {
+                case 0:
+                    game_state = 1;
+                    start_btn.Enabled = false;
+                    timer.Start();
+                    break;
+                default:
+                    log_textbox.Text += "unexpected game_state in start_btn_Click\r\n";
+                    break;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            switch (game_state)
+            {
+                case 1:
+                    stored_time += timer.Interval;
+                    if (stored_time > cube_interval)
+                    {
+                        // generate cubes
+                        char basic = 'a', c;
+
+                        if (r.Next(10) == 0)
+                            basic = 'A';
+
+                        c = (char)(r.Next(26) + basic);
+
+                        Label cube = new Label();
+                        cube.BackColor = Color.Black;
+                        cube.ForeColor = Color.White;
+                        cube.Font = new Font(cube.Font.Name, cube_font_size, FontStyle.Bold);
+                        cube.Size = cube_size;
+                        cube.Text = c.ToString();
+
+                        Controls.Add(cube);
+
+                        cube.Top = 0;
+                        cube.Left = r.Next(deadline_textbox.Left, deadline_textbox_cube_right);
+
+                        if (current_cubes.ContainsKey(c))
+                            current_cubes[c].Add(cube);
+                        else
+                            current_cubes[c] = new List<Label>() { cube };
+
+                        stored_time = 0; 
+                    }
+
+                    // move those cubes
+                    foreach (KeyValuePair<char, List<Label>> pair in current_cubes)
+                        for (int i = 0; i < pair.Value.Count; ++i)
+                        {
+                            pair.Value[i].Top += (int)gravity;
+
+                            if (pair.Value[i].Bottom >= deadline_textbox.Top)
+                            {
+                                // bomb
+                                if (HP == 1)
+                                {
+                                    // avoid the error in foreach
+                                    --HP;
+                                    return;
+                                }
+
+                                --HP;
+                                
+                                pair.Value[i].Dispose();
+                                pair.Value.RemoveAt(i);
+                                --i;
+                            }
+                        }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void check_typing(char c)
+        {
+            try
+            {
+                if (current_cubes.ContainsKey(c))
+                {
+                    current_cubes[c][0].Dispose();
+                    current_cubes[c].RemoveAt(0);
+
+                    if (current_cubes[c].Count == 0)
+                        current_cubes.Remove(c);
+
+                    ++Score;
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (game_state)
+            {
+                case 1:
+
+                    // capital
+                    // 16 is the "shift" code in int
+                    if (e.Shift && e.KeyValue != 16)
+                        check_typing((char)e.KeyValue);
+
+                    // small
+                    if (!e.Shift)
+                        check_typing((char)(e.KeyValue + 32));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void restart_btn_Click(object sender, EventArgs e)
+        {
+            Form1_Load(this, null);
+        }
+
+        private void name_btn_Click(object sender, EventArgs e)
+        {
+            Player_name = name_textbox.Text;
+        }
+
+        string score_log(string n, int s, System.DateTime t)
+        {
+            return "player " + n + " get " + s.ToString() + " points at "
+                + string.Format("{0:yyyy/M/d HH:mm:ss}", t) + "\r\n";
+        }
+        
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            log_textbox.Text = "";
+            FileInfo finfo = new FileInfo(score_path);
+            finfo.Delete();
+        }
+
+        string Player_name
+        {
+            get { return player_name; }
+            set
+            {
+                player_name = value;
+                name_label.Text = "player: " + player_name;
+            }
+        }
+
+        int Score
+        {
+            get { return score; }
+            set
+            {
+                score = value;
+                score_label.Text = "score:\r\n" + score.ToString();
+
+                gravity = (00.2f + score * 0.005f) * timer.Interval;
+            }
+        }
+
+        int HP
+        {
+            get { return hp; }
+            set
+            {
+                hp = value;
+                HP_label.Text = "HP:\r\n" + hp.ToString();
+
+                if (hp == 0 && game_state == 1)
+                {
+                    // game over
+
+                    // show score log
+                    log_textbox.Text += score_log(Player_name, Score, System.DateTime.Now);
+
+                    // store the score log
+                    StreamWriter sw = new StreamWriter(score_path, true);
+                    sw.WriteLine(Player_name + " " + Score.ToString() + " "
+                        + string.Format("{0:yyyy/M/d HH:mm:ss}", System.DateTime.Now));
+                    sw.Close();
+
+                    timer.Stop();
+
+                    Form1_Load(this, null);
+                }
+
+            }
+        }
+
+        Dictionary<char, List<Label>> current_cubes;
+
+        Random r;
+        Size cube_size = new Size(30, 30);
+        string score_path = @"score.txt";
+        string player_name;
+        float gravity;
+        int init_HP = 10, cube_interval = 499, cube_font_size = 20;
+        int hp, score, game_state, stored_time;
+        int deadline_textbox_cube_right;
+    }
+}
